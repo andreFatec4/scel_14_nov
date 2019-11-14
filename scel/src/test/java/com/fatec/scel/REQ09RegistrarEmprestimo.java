e43df53bd0d71e3af8ba114bc3fd27caa0fb19d4
aluno <aluno@L2M13.IPIRANGA.FATEC>
2019-11-14 16:21:35 -0200

    Criação da classe Emprestimo

---
+++ scel/src/test/java/com/fatec/scel/REQ09RegistrarEmprestimo.java
@@ -0,0 +1,14 @@
+package com.fatec.scel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class REQ09RegistrarEmprestimo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
